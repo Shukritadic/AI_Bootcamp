e0b48fff55bbf58c50661ff403076ef6d2473b81
shukritadic <shukritadicha@gmail.com>
2024-02-10 15:43:35 +0300

    recent updates

---
+++ python_essentials/New_line_and _backslash.py
@@ -0,0 +1,1 @@
+print("my email address is: shukritadicha@gmail.com")
---
+++ python_essentials/Untitled-2.py
@@ -0,0 +1,10 @@
+print("my name is shukri")
+print("\n")
+
+print("my name is shukri" +" "+ " and I live in Kilimani")
+name = "Datacamp"
+type_of_company = "Educational"
+print(F"{name} is an {type_of_company} company.")
+print("shukri","Ali", "john",  sep=" ")
+print("shukri","Ali", "john",  end=" ")
+print("who are you"* 3)
