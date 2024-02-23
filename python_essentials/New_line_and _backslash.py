a2867df4da5d4e912d2d4eb6905bebf7ca88d17c
shukritadic <shukritadicha@gmail.com>
2024-02-23 11:03:22 +0000

    Data cleaning files

--- python_essentials/New_line_and _backslash.py
+++ python_essentials/New_line_and _backslash.py
@@ -1,1 +1,7 @@
-print("my email address is: shukritadicha@gmail.com")
+import psycopg2 
+import pandas as pd
+#create connection to the database
+conn= psycopg2.connect(database ='Hospital',
+                       user= 'Postgres',
+                       password = 'postgres',
+                       port= '5042')
